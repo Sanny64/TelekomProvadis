23836f418520fe9070f89006add75033adad7f00
Sammynator <sgerules@gmail.com>
2023-10-26 14:59:38 +0200

    added colorful text

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let attempts = 0;
 function checkGuess() {
   const guessedNumber = parseInt(document.getElementById("guessInput").value);
 
+
   if (isNaN(guessedNumber)) {
     document.getElementById("message").textContent = "Bitte gib eine gültige Nummer ein.";
   } else {
@@ -11,13 +12,19 @@ function checkGuess() {
 
     if (guessedNumber === randomNumber) {
       document.getElementById("message").textContent = `Glückwunsch! Du hast die richtige Zahl (${randomNumber}) in ${attempts} Versuchen erraten.`;
+      message.style.color = 'green';
+      setGameover();
     } else if (guessedNumber < randomNumber) {
       document.getElementById("message").textContent = "Die Zahl ist zu niedrig. Versuche es erneut.";
+      message.style.color = 'red';
+      setGameover();
     } else {
       document.getElementById("message").textContent = "Die Zahl ist zu hoch. Versuche es erneut.";
+      message.style.color = 'red';
+      setGameover();
     }
 
-
+    
     if (attempts === 3) {
       randomNumber = Math.floor(Math.random() * 10) + 1;
       attempts = 0;

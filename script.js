a3c5a636a12af298dcad7f95d4ce97cc81652361
Sanny <santino.pia04@gmx.de>
2025-10-06 11:12:55 +0200

    Added favicon, added color logic to game, added number already guessed

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ inputField.addEventListener("keypress", event => {
   } 
 });
 
-
+// yes, i am aware you can just console.log the random number in the browser console - I don't care XD
 let randomNumber = Math.floor(Math.random() * 10) + 1;
 let attempts = 0;
+let guessedNumbers = [];
 
 function checkGuess() {
   const guessedNumber = parseInt(inputField.value);
@@ -16,27 +17,51 @@ function checkGuess() {
   if (isNaN(guessedNumber) || (guessedNumber > 10) || (guessedNumber <= 0)) {
     document.getElementById("message").textContent = "Bitte gib eine gültige Nummer ein.";
     message.style.color = 'red';
+  } else if (guessedNumbers.includes(guessedNumber)) {
+    
+    document.getElementById("message").textContent = `Du hast ${guessedNumber} bereits versucht! Versuche eine andere Zahl.`;
+    message.style.color = 'orange';
   } else {
     attempts++;
+    guessedNumbers.push(guessedNumber); 
 
     if (guessedNumber === randomNumber) {
-      document.getElementById("message").textContent = `Glückwunsch! Du hast die richtige Zahl ${randomNumber} in ${attempts} Versuchen erraten.`;
+      document.getElementById("message").textContent = `Glückwunsch! Du hast die Zahl ${randomNumber} in ${attempts} Versuchen richtig erraten.`;
       message.style.color = 'green';
-      
-    } else if (guessedNumber < randomNumber) {
-      document.getElementById("message").textContent = "Die Zahl ist zu niedrig. Versuche es erneut.";
-      message.style.color = 'red';
-      
-    } else {
-      document.getElementById("message").textContent = "Die Zahl ist zu hoch. Versuche es erneut.";
-      message.style.color = 'red';
-      
-    } 
-    
-    if (attempts === 3) {
       randomNumber = Math.floor(Math.random() * 10) + 1;
       attempts = 0;
+      guessedNumbers = []; 
+      
+    } else if ((guessedNumber < randomNumber)){
+      document.getElementById("message").textContent = "Die Zahl ist zu niedrig. Versuche es erneut.";
+      switch (attempts) {
+        case 1: 
+          message.style.color = 'yellow';
+          break;
+        case 2: 
+          message.style.color = 'orange';
+          break;
+        default: message.style.color = 'black';
+      }
+    } else {
+      document.getElementById("message").textContent = "Die Zahl ist zu hoch. Versuche es erneut.";
+      switch (attempts) {
+        case 1: 
+          message.style.color = 'yellow';
+          break;
+        case 2: 
+          message.style.color = 'orange';
+          break;
+        default: message.style.color = 'black';
+      }
+    } 
+    
+    if ((attempts === 3) && (guessedNumber != randomNumber)) {
+      randomNumber = Math.floor(Math.random() * 10) + 1;
+      attempts = 0;
+      guessedNumbers = [];
       document.getElementById("message").textContent = "3 Fehlversuche. Zahl wird neu generiert."
+      message.style.color = 'red';
     }
   }
 }

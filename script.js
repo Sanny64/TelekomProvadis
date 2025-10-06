a2d7696f211a9e233008e8446ac980475baccbb6
Sanny <santino.pia04@gmx.de>
2025-10-06 11:45:26 +0200

    Added helper functions, added win message for first try/more tries

--- script.js
+++ script.js
@@ -5,14 +5,13 @@ inputField.addEventListener("keypress", event => {
   } 
 });
 
-// yes, i am aware you can just console.log the random number in the browser console - I don't care XD
+// yes, i am aware you can just console.log the randomNumber in the browser console - I don't care XD
 let randomNumber = Math.floor(Math.random() * 10) + 1;
 let attempts = 0;
 let guessedNumbers = [];
 
 function checkGuess() {
   const guessedNumber = parseInt(inputField.value);
-
 
   if (isNaN(guessedNumber) || (guessedNumber > 10) || (guessedNumber <= 0)) {
     document.getElementById("message").textContent = "Bitte gib eine gültige Nummer ein.";
@@ -23,45 +22,49 @@ function checkGuess() {
     message.style.color = 'orange';
   } else {
     attempts++;
-    guessedNumbers.push(guessedNumber); 
+    guessedNumbers.push(guessedNumber);
 
     if (guessedNumber === randomNumber) {
-      document.getElementById("message").textContent = `Glückwunsch! Du hast die Zahl ${randomNumber} in ${attempts} Versuchen richtig erraten.`;
+      switch (attempts) {
+        case 1:
+          document.getElementById("message").innerHTML = `Glückwunsch!<br><br>Du hast die Zahl beim ersten Versuch erraten.`;
+        break;
+        default:
+          document.getElementById("message").innerHTML = `Glückwunsch!<br><br>Du hast die Zahl in ${attempts} Versuchen erraten.`;
+      }
       message.style.color = 'green';
-      randomNumber = Math.floor(Math.random() * 10) + 1;
-      attempts = 0;
-      guessedNumbers = []; 
+      resetGame()
       
     } else if ((guessedNumber < randomNumber)){
       document.getElementById("message").textContent = "Die Zahl ist zu niedrig. Versuche es erneut.";
-      switch (attempts) {
-        case 1: 
-          message.style.color = 'yellow';
-          break;
-        case 2: 
-          message.style.color = 'orange';
-          break;
-        default: message.style.color = 'black';
-      }
+      setStyle(attempts)
     } else {
       document.getElementById("message").textContent = "Die Zahl ist zu hoch. Versuche es erneut.";
-      switch (attempts) {
-        case 1: 
-          message.style.color = 'yellow';
-          break;
-        case 2: 
-          message.style.color = 'orange';
-          break;
-        default: message.style.color = 'black';
+      setStyle(attempts)
       }
     } 
     
     if ((attempts === 3) && (guessedNumber != randomNumber)) {
-      randomNumber = Math.floor(Math.random() * 10) + 1;
-      attempts = 0;
-      guessedNumbers = [];
       document.getElementById("message").textContent = "3 Fehlversuche. Zahl wird neu generiert."
       message.style.color = 'red';
+      resetGame();
     }
+}
+
+function setStyle (attempts) {
+  switch (attempts) {
+    case 1: 
+      message.style.color = 'yellow';
+      break;
+    case 2: 
+      message.style.color = 'orange';
+      break;
+    default: message.style.color = 'black';
   }
+}
+
+function resetGame () {
+  attempts = 0;
+  guessedNumbers = [];
+  randomNumber = Math.floor(Math.random() * 10) + 1;
 }
